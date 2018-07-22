5835cf013cf8bbbd7ef0dc23409d270e18b2b424
Matt <37934066+moracle38@users.noreply.github.com>
2018-07-21 22:12:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,9 @@ bot.onText(/\/instalacao/, (msg) => {
 bot.onText(/\/gnome/, (msg) => {
   bot.sendMessage(msg.chat.id, message.gnomeAnswer, { parse_mode: 'Markdown' })
 })
+bot.onText(/\/kde/, (msg) => {
+  bot.sendMessage(msg.chat.id, message.kdeAnswer, { parse_mode: 'Markdown' })
+})
 bot.onText(/\/socorro/, (msg) => {
   bot.sendMessage(msg.chat.id, message.erico, { parse_mode: 'Markdown' })
 })
