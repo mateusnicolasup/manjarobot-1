7e058e1059d367acab1a74a3e097ba47d625042f
mateusnicolasup <41706792+mateusnicolasup@users.noreply.github.com>
2018-07-25 19:05:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,9 @@ bot.onText(/\/instalacao/, (msg) => {
 bot.onText(/\/gnome/, (msg) => {
   bot.sendMessage(msg.chat.id, message.gnomeAnswer, { parse_mode: 'Markdown' })
 })
+bot.onText(/\/rules/, (msg) => {
+  bot.sendMessage(msg.chat.id, message.rules, { parse_mode: 'Markdown' })
+})
 bot.onText(/\/wpsoffice/, (msg) => {
   bot.sendMessage(msg.chat.id, message.wps, { parse_mode: 'Markdown' })
 })
